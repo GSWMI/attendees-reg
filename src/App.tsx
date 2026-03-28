5b1045d325b276fe3bfc627f06eb4c5db30a5913
moyinfesobi <moyinoluwafesobi7@gmail.com>
2026-03-28 08:53:40 +0100

    rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import EventLanding from "./pages/EventLanding";
-import RegisterPayment from "./pages/RegisterPayment";
-import PleaseWait from "./pages/PleaseWait";
-import Registered from "./pages/Registered";
+
 
 
 function App() {
@@ -11,10 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EventLanding />} />
-        <Route path="/register" element={<RegisterPayment />} />
-        <Route path="/wait" element={<PleaseWait />} />
-        <Route path="/registered" element={<Registered />} />
+       
       </Routes>
     </BrowserRouter>
   )
